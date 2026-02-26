cac3948988383209e9d4ed3cbe196828219542b9
peterkruhoffer <peter.kruhoffer@gmail.com>
2026-02-26 20:19:20 +0100

    set bg color on body and formating

--- src/main.tsx
+++ src/main.tsx
@@ -3,24 +3,24 @@ import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({
-	routeTree,
-	defaultPreload: "intent",
-	scrollRestoration: true,
+  routeTree,
+  defaultPreload: "intent",
+  scrollRestoration: true,
 });
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
 
 const rootElement = document.getElementById("app");
 
 if (!rootElement) {
-	throw new Error('Root element with id "app" was not found.');
+  throw new Error('Root element with id "app" was not found.');
 }
 
 if (!rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement);
-	root.render(<RouterProvider router={router} />);
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<RouterProvider router={router} />);
 }
